ae1958f323cf999605b50c0f602e99677b391acf
Oriana <98231498+OrianaLand@users.noreply.github.com>
2025-03-12 17:24:43 -0300

    Create basic HTML calculator

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let firstNum = "";
+let seconnNum = "";
+let operator = "";
+
+function operate(op, a, b) {
+  switch (op) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return substract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
